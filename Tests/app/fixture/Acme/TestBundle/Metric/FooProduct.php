43ad1f94c4abba77450a6d27c804b129b0409824
Linas Mockus <linas.mockus@ongr.io>
2015-01-20 08:05:16 +0200

    CS fixes.

--- Tests/app/fixture/Acme/TestBundle/Metric/FooProduct.php
+++ Tests/app/fixture/Acme/TestBundle/Metric/FooProduct.php
@@ -8,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace ONGR\MonitoringBundle\Tests\app\fixture\Acme\TestBundle\Metric;
 
 use ONGR\ElasticsearchBundle\DSL\Query\MatchAllQuery;
